9030bb7553ed0ec6a2a202f798a25a085f196137
Andrew Craigie <andrew@craigie.co>
2018-09-09 16:01:23 +0100

    Removes Coins model from app

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,11 @@
 const Cryptotracker = require('./models/cryptotracker.js');
 const AddCoinView = require('./views/add_coin_view.js');
 const PortfolioiListView = require('./views/portfolio_list_view.js');
-const Coins = require('./models/coins.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
   const apiUrl = 'https://api.coinmarketcap.com/v2/ticker/';
   const databaseUrl = "http://localhost:3000/api/cryptotracker";
-
-
 
   const cryptotracker = new Cryptotracker(databaseUrl, apiUrl);
   cryptotracker.bindEvents();
@@ -16,9 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const addCoinForm = document.querySelector('form#add-coin-form');
   const addCoinView = new AddCoinView(addCoinForm);
   addCoinView.bindEvents();
-
-  // const coins = new Coins(coinListUrl);
-  // coins.bindEvents();
 
   const portfolioContainer = document.querySelector('div#portfolio-list');
   const portfolioListView = new PortfolioiListView(portfolioContainer);
