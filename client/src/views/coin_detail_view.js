f6a8ef296e1a96d1434d334cd6633974b091563a
Andrew Craigie <andrew@craigie.co>
2018-09-12 09:41:01 +0100

    Update coin detail view

--- client/src/views/coin_detail_view.js
+++ client/src/views/coin_detail_view.js
@@ -8,9 +8,20 @@ const CoinDetailView = function(container){
   this.container = container;
   this.coinData = null;
 
+  this.coinDetailsGroup = null;
+  this.controlsGroup = null;
+
   this.chartDiv = null;
+  this.dataDiv = null;
+
+  this.updateButton = null;
+  this.quantityControl = null;
+
+
   this.isDeleteMessage = false;
+
   this.theme = null;
+
 
 };
 
@@ -27,7 +38,9 @@ CoinDetailView.prototype.bindEvents = function(){
 
       this.isDeleteMessage = true;
       this.renderDeleteMessage();
-      //console.log(event.detail);
+
+
+      console.log(event.detail);
     });
 
     PubSub.subscribe('Themes:theme-available', (event) => {
@@ -35,10 +48,117 @@ CoinDetailView.prototype.bindEvents = function(){
       this.render();
     });
 
+
 };
 
+CoinDetailView.prototype.handleUpdate = function(event){
+
+    const quantity = this.quantityControl.value;
+    const coinId = this.coinData.portfolioId;
+
+    const coin = {
+      symbol: this.coinData.symbol,
+      id: coinId,
+      quantity: quantity
+    };
+
+    PubSub.publish('CoinDetailView:coin-updated', coin);
+};
+
+CoinDetailView.prototype.handleDelete = function(event){
+
+    const status = confirm(`Are you sure you want to delete ${this.coinData.portfolioQuantity} ${this.coinData.name} from your portfolio?`);
+
+    if (status){
+     g PubSub.publish('CoinDetailView:delete-coin', this.coinData.portfolioId);
+    }
+
+};
+
+CoinDetailView.prototype.handleQuantityChange = function(event){
+  this.updateButton.disabled = false;
+};
+
+CoinDetailView.prototype.makeControlsGroup = function(){
+
+  this.controlsGroup = element.make({
+    tag: 'div',
+    attribs: {
+      id: 'coin-detail-control-group'
+    }
+  });
+
+  const deleteButton = element.make({
+    tag: 'button',
+    attribs: {
+      id: 'coin-detail-delete-button'
+    },
+    content: 'Delete Coin'
+  });
+  deleteButton.addEventListener('click', this.handleDelete.bind(this));
+  this.controlsGroup.appendChild(deleteButton);
+
+  const quantityLabel = element.make({
+    tag: 'p',
+    attribs: {
+      id: 'coin-detail-quantity-label'
+    },
+    content: 'Quantity'
+  });
+  this.controlsGroup.appendChild(quantityLabel);
+
+  this.quantityControl = element.make({
+    tag: 'input',
+    attribs: {
+      id: 'coin-detail-quantity-input',
+      type: 'number',
+      min: 0,
+      step: 0.01,
+      value: this.coinData.portfolioQuantity
+    }
+  });
+  this.quantityControl.required = true;
+  this.quantityControl.addEventListener('change', this.handleQuantityChange.bind(this))
+  this.controlsGroup.appendChild(this.quantityControl);
+
+  this.updateButton  = element.make({
+    tag: 'button',
+    attribs: {
+      id: 'coin-detail-update-button'
+    },
+    content: 'Update Quantity'
+  });
+  this.updateButton .disabled = true;
+  this.updateButton .addEventListener('click', this.handleUpdate.bind(this));
+  this.controlsGroup.appendChild(this.updateButton);
+
+  this.container.appendChild(this.controlsGroup);
+
+};
+
+CoinDetailView.prototype.makeChartDiv = function () {
+  this.chartDiv = element.make({
+    tag: 'div',
+    attribs: {
+      id: 'coin-detail-view-chart-div'
+    }
+  });
+  this.container.appendChild(this.chartDiv);
+};
+
+CoinDetailView.prototype.makeDataDiv = function () {
+  this.dataDiv = element.make({
+    tag:'div',
+    attribs: {
+      id: 'coin-detail-view-data-div'
+    }
+  });
+    this.container.appendChild(this.dataDiv);
+};
 
 CoinDetailView.prototype.renderDeleteMessage = function(){
+
+  console.log('CoinDetailView: renderDeleteMessage', this.isDeleteMessage);
 
   const container = this.container;
   element.clear(container);
@@ -52,34 +172,82 @@ CoinDetailView.prototype.renderDeleteMessage = function(){
   });
   container.appendChild(tempElement)
 
-  setTimeout(function(){
-     tempElement.classList.remove('temp-element');
-     tempElement.classList.add('temp-element-fade');
-   }, 600);
-
-   setTimeout(function(){
-      container.removeChild(tempElement);
-    }, 2000);
+  container.classList.toggle("is-active");
 
 };
 
-CoinDetailView.prototype.renderChart = function () {
+CoinDetailView.prototype.render = function(){
+  element.clear(this.container);
+  if (this.coinData) {
+    this.makeHeader();
+    this.makeChartDiv();
+    this.makeDataDiv();
 
+    this.renderData();
+    this.renderChart();
+
+    this.makeControlsGroup();
+  }
+
+
+};
+
+CoinDetailView.prototype.renderData = function () {
+  const priceElement = element.make({
+    tag: 'p',
+    attribs: {
+      class:'price-para'
+    },
+    content: `Unit Price : $ ${this.coinData.quotes.USD.price}`
+  });
+  this.dataDiv.appendChild(priceElement);
+
+
+  const rankElement = element.make({
+    tag: 'p',
+    attribs: {
+      class:'rank-para'
+    },
+    content: `Coin Rank : ${this.coinData.rank}`
+  });
+  this.dataDiv.appendChild(rankElement);
+
+
+  const valueElement = element.make({
+    tag: 'p',
+    attribs: {
+      class:'value-para'
+    },
+    content: `Total Value : $ ${this.coinData.portfolioValue}`
+  });
+  this.dataDiv.appendChild(valueElement);
+
+  const marketCapElement = element.make({
+    tag: 'p',
+    attribs: {
+      class:'value-para'
+    },
+    content: `Market Cap : $ ${this.coinData.quotes.USD.market_cap}`
+  });
+
+  this.dataDiv.appendChild(marketCapElement);
+};
+
+CoinDetailView.prototype.renderChart = function () {
   Highcharts.theme = this.theme;
   Highcharts.setOptions(Highcharts.theme);
-
   const categories = this.coinData.historicalData.map((data) => {
     return data.timeStamp;
   });
   const chartData = this.coinData.historicalData.map((data) => {
     return data.close;
   });
-  const chart = Highcharts.chart(this.container, {
+  const chart = Highcharts.chart(this.chartDiv, {
     chart: {
       type: 'line'
     },
     title: {
-      text: `${this.coinData.name} Performance'`
+      text: 'Coin Performance'
     },
     xAxis: {
       categories: categories
@@ -90,23 +258,41 @@ CoinDetailView.prototype.renderChart = function () {
       }
     },
     series: [{
-      type: 'area',
       name: 'Coin Value',
       data: chartData
     }]
   });
-
 };
 
-CoinDetailView.prototype.render = function(){
+CoinDetailView.prototype.makeHeader = function () {
 
-  element.clear(this.container);
+  const imageElement = element.make({
+    tag: 'img',
+    attribs: {
+      class: 'coin-image',
+      src: `/images/${this.coinData.website_slug}.png`
+    }
 
-  if (this.coinData) {
-    this.renderChart();
-  }
+  });
+  this.container.appendChild(imageElement);
 
+
+  setTimeout(function(){
+     container.classList.toggle("is-active");
+     console.log('timeout');
+   }, 300);
+
+  const headerElement = element.make({
+    tag: 'h2',
+    attribs: {
+      class: 'coin-header'
+    },
+    content: `${this.coinData.name} (${this.coinData.symbol})`
+  });
+  this.container.appendChild(headerElement);
 };
+
+
 
 
 module.exports = CoinDetailView;
